4f6b7da07d2953a18cafc6571274272ae93be941
KikeM22 <kikemmen22@gmail.com>
2022-06-20 13:54:30 -0500

    first commit

---
+++ co.php
@@ -0,0 +1,12 @@
+<?php
+error_reporting(0);
+define('RUTA', 'https://tesyredes.herokuapp.com/');
+// nombre de base de datos
+$bd_config = array(
+    'db_name' => 'bsmuaku4qtaz0rptiirp',
+    'user' => 'urejjvtayrp92wey',
+    'pass' => 'YVPrG4Km9xeX0yLU1ri9'    
+);
+
+
+ ?>
